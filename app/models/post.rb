2fe056a7c3a6bbdbbd7147c8cd8fb5fe8290f9cb
Anas Mohamed <amohamed@ualberta.ca>
2021-05-03 07:09:04 -0600

    posts page shows preview. fixed webpack manifest issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many_attached :images do |attachable|
-    attachable.variant :preview, resize: "480x480"
-    attachable.variant :thumb, resize: "120x120"
-  end
+  has_many_attached :images
 end
