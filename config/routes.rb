607860f47845d1c46436c267e62b74e9a1d1a46b
Anas Mohamed <amohamed@ualberta.ca>
2021-05-07 11:27:59 -0600

    added loading skeleton for posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
+
   resources :posts do
     member do
       delete :delete_image
