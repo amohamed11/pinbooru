aba1842235d292a187c4bf87c50c2b94f683c500
Anas Mohamed <amohamed@ualberta.ca>
2021-05-08 02:47:03 -0600

    centered grid on index page. fixed menu display

--- app/javascript/packs/menu_button.js
+++ app/javascript/packs/menu_button.js
@@ -1,11 +1,7 @@
 function switchMenuDisplay() {
   const menuItems = document.getElementById("menu-items")
 
-  if (menuItems.style.display === "none") {
-    menuItems.style.display = "block"
-  } else {
-    menuItems.style.display = "none"
-  }
+  menuItems.style.display = menuItems.style.display === "block" ? "none" : "block" 
 }
 
 const menuButton = document.getElementById("menu-button")
